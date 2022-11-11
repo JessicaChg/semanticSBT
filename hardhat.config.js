1a3b4b616b471a6aada597ea88c72d8c23db4500
bornel1 <105546545+bornel1@users.noreply.github.com>
2022-11-12 01:35:36 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require('@openzeppelin/hardhat-upgrades');
 
-const INFURA_PROJECT_ID = "";
-const PRIVATE_KEY = "";
+// Replace "INFURA PROJECT ID" with your INFURA project id
+// Go to https://infura.io/, sign up, create a new App in its dashboard, and replace "KEY" with its key
+const INFURA_PROJECT_ID = "INFURA INFURA PROJECT ID";
+
+// Replace "PRIVATE KEY" with your account private key
+// To export your private key from Metamask, open Metamask and go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const PRIVATE_KEY = "PRIVATE KEY";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
