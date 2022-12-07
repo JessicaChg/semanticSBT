42225bb23348b87e066ca1230768fb0b15dc2026
bornel1 <105546545+bornel1@users.noreply.github.com>
2022-12-08 00:03:32 +0800

    Update SemanticSBT.test.js
    add the annotation for this test case

--- test/SemanticSBT.test.js
+++ test/SemanticSBT.test.js
@@ -1,7 +1,8 @@
+/*
+* These cases are for testing the methods of Semantic SBT demo contract
+*/
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
-
-
 
 const name = 'example SBT';
 const symbol = 'SBT';
@@ -14,7 +15,15 @@ const addressPredicate = ['addressPredicate', 2];
 const subjectPredicate = ['subjectPredicate', 3];
 const blankNodePredicate = ['blankNodePredicate', 4];
 
-
+/*
+* Before Mint SBT, should initial the parameters of this contract. In this step, we prepare the element of semantic SBT
+* @param name The name for the Semantic SBT
+* @param name The symbol for the Semantic SBT
+* @param baseURI The URI may point to a JSON file that conforms to the "ERC721Metadata JSON Schema".
+* @param schemaURI The URI of the contract witch point to a JSON file that conforms to the "ISemanticMetadata Metadata JSON Schema".
+* @param [className] The array of class name which are used for define the "SUBJECT" of SPO 
+* @param [className] The array of five data types of predicates which are used for define the "PREDICATE" of SPO 
+*/
 describe("SemanticSBT contract", function () {
   async function deployTokenFixture() {
     const [owner, addr1, addr2] = await ethers.getSigners();
@@ -31,33 +40,42 @@ describe("SemanticSBT contract", function () {
     return { semanticSBT, owner, addr1, addr2 };
   }
 
-
+  // check semanticSBT belong this contract owner
   it("owner", async function () {
     const { semanticSBT, owner } = await loadFixture(deployTokenFixture);
     expect(await semanticSBT.owner()).to.equal(owner.address);
   });
-
+  // make sure contract owner can mint SBT
   it("minter", async function () {
     const { semanticSBT, owner } = await loadFixture(deployTokenFixture);
     expect(await semanticSBT.minters(owner.address)).to.equal(true);
   });
 
+  // make sure the name of semantic SBT setup up as expected
   it("name", async function () {
     const { semanticSBT } = await loadFixture(deployTokenFixture);
     expect(await semanticSBT.name()).to.equal(name);
   });
 
+  // make sure the symbol of semantic SBT setup up as expected
   it("symbol", async function () {
     const { semanticSBT } = await loadFixture(deployTokenFixture);
     expect(await semanticSBT.symbol()).to.equal(symbol);
   });
 
+  // make sure the schemaURI of semantic SBT setup up as expected
   it("schemaURI", async function () {
     const { semanticSBT } = await loadFixture(deployTokenFixture);
     expect(await semanticSBT.schemaURI()).to.equal(schemaURI);
   });
 
 
+  /*
+  * Below are the test cases for mint and burn semantic SBT.
+  * Due to predicate in contract has five data types: int, string, address, subject and blankNode
+  * the fist five cases are belonging to the respective data type
+  * the last one is for the unions of five data types 
+  */
   describe("mint and burn", function () {
     it("mint with only intPredicate,then burn", async function () {
       const { semanticSBT, owner, addr1 } = await loadFixture(deployTokenFixture);
